ed8b75d649ac36754134eccef7f2da0dd05414fb
rafael16k <leafar.aureliano@gmail.com>
2021-12-27 19:46:22 -0300

    finalizando projeto de conversor de moedas

---
+++ README.md
@@ -0,0 +1,22 @@
+<h1 align="center">
+    <img src="assets/img/logo/logo2.png" width="150">
+</h1>
+
+<img src="assets/img/bg/gif-conversor.gif">
+
+###  Sobre
+
+**Conversor de moedas:** Ele pode converter dólar, libra, euro e Renminbi em real. Ele também mostra o valor da moeda a baixo
+
+---
+GitHub Pages: 
+ 
+---
+### 💻 Tecnologia utilizada
+
+- HTML 5
+- CSS 3
+- JAVASCRIPT
+
+---
+
---
+++ assets/js/script.js
@@ -0,0 +1,89 @@
+
+var clickMoedas = document.querySelector('.area-moedas--bandeiras');
+
+
+
+   
+
+
+    clickMoedas.addEventListener('click', (event)=>{
+        converMoedas[event.target.id]();
+        console.log(converMoedas)
+    });
+
+    const converMoedas = {
+        'dolar': ()=> { 
+            const valorDigitado = parseFloat(document.getElementById('valor').value);
+            const resultadoDaConver = document.querySelector('.convertido');
+            const valorMoeda = document.querySelector('.valorDaMoeda');
+
+            const moeda = 5.62
+
+            if(valorDigitado > 0){
+            const valor = valorDigitado * moeda;
+            resultadoDaConver.innerHTML = "R$:" + valor.toFixed(2);
+            valorMoeda.innerHTML = "Valor do Dólar " + moeda;
+            }else{
+                resultadoDaConver.innerHTML = "Digite um valor para converter";
+            }
+         },
+         'libras': ()=> { 
+            var valorDigitado = parseFloat(document.getElementById('valor').value);
+            const resultadoDaConver = document.querySelector('.convertido');
+            const valorMoeda = document.querySelector('.valorDaMoeda');
+
+            const moeda = 7.76;
+
+            if(valorDigitado > 0){
+            var valor = valorDigitado * moeda;
+            resultadoDaConver.innerHTML = "R$:" + valor.toFixed(2);
+            valorMoeda.innerHTML = "Valor da Libras " + moeda;
+            }else{
+                resultadoDaConver.innerHTML = "Digite um valor para converter";
+            }
+         },
+         'euro': ()=> { 
+            var valorDigitado = parseFloat(document.getElementById('valor').value);
+            const resultadoDaConver = document.querySelector('.convertido');
+            const valorMoeda = document.querySelector('.valorDaMoeda');
+
+            const moeda = 6.64;
+
+            if(valorDigitado > 0){
+            var valor = valorDigitado * moeda;
+            resultadoDaConver.innerHTML = "R$:" + valor.toFixed(2);
+            valorMoeda.innerHTML = "Valor do Euro " + moeda;
+            }else{
+                resultadoDaConver.innerHTML = "Digite um valor para converter";
+            }
+         },
+         'renmi': ()=> { 
+            var valorDigitado = parseFloat(document.getElementById('valor').value);
+            const resultadoDaConver = document.querySelector('.convertido');
+            const valorMoeda = document.querySelector('.valorDaMoeda');
+
+            const moeda = 0.86;
+
+            if(valorDigitado > 0){
+            var valor = valorDigitado * moeda;
+            resultadoDaConver.innerHTML = "R$:" + valor.toFixed(2);
+            valorMoeda.innerHTML = "Valor do Renminbi " + moeda;
+            }else{
+                resultadoDaConver.innerHTML = "Digite um valor para converter";
+            }
+         }
+    }
+
+
+    //dava pra fazer assim também 
+    /*function libras () {
+    let resultadoDaConver = document.querySelector('.convertido');
+    let valorDigitado = parseFloat(document.getElementById('valor').value);
+
+    let valor = valorDigitado * 5.55 ;
+
+    let resultado = valor.toFixed(2);
+
+    resultadoDaConver.innerHTML = resultado;
+
+}*/
